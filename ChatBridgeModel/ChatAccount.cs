3306a0854de0f69de91b2fb94ea9c04814530f5d
kheiakiyama <kheiakiyama@gmail.com>
2016-04-19 13:58:31 +0900

    create chat account model

---
+++ ChatBridgeModel/ChatAccount.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBridgeModel
+{
+    public class ChatAccount : TableEntity
+    {
+        public string OwnerId { get; set; }
+        public string Id { get; set; }
+        public string ChannelId { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
